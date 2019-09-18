066c1c51da2f4ed7ba97b1954007282621d91832
blangis <blangis@A4>
2019-09-18 16:45:27 +0200

    APPTarot 0.3

---
+++ MFCApplication1/MFCApplication1/ComptagedePointetdeBOUTS.cpp
@@ -0,0 +1,34 @@
+// ComptagedePointetdeBOUTS.cpp : fichier d'implémentation
+//
+
+#include "stdafx.h"
+#include "MFCApplication1.h"
+#include "ComptagedePointetdeBOUTS.h"
+#include "afxdialogex.h"
+
+
+// Boîte de dialogue ComptagedePointetdeBOUTS
+
+IMPLEMENT_DYNAMIC(ComptagedePointetdeBOUTS, CDialogEx)
+
+ComptagedePointetdeBOUTS::ComptagedePointetdeBOUTS(CWnd* pParent /*=NULL*/)
+	: CDialogEx(IDD_COMPTAGEDEPOINTETDEBOUTS, pParent)
+{
+
+}
+
+ComptagedePointetdeBOUTS::~ComptagedePointetdeBOUTS()
+{
+}
+
+void ComptagedePointetdeBOUTS::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(ComptagedePointetdeBOUTS, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// Gestionnaires de messages de ComptagedePointetdeBOUTS
---
+++ MFCApplication1/MFCApplication1/ComptagedePointetdeBOUTS.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+// Boîte de dialogue ComptagedePointetdeBOUTS
+
+class ComptagedePointetdeBOUTS : public CDialogEx
+{
+	DECLARE_DYNAMIC(ComptagedePointetdeBOUTS)
+
+public:
+	ComptagedePointetdeBOUTS(CWnd* pParent = NULL);   // constructeur standard
+	virtual ~ComptagedePointetdeBOUTS();
+
+// Données de boîte de dialogue
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_COMPTAGEDEPOINTETDEBOUTS };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // Prise en charge de DDX/DDV
+
+	DECLARE_MESSAGE_MAP()
+};
