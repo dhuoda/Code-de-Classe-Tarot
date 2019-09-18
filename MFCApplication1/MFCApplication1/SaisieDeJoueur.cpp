dcd8eb9c96117b472cfb3f6d0e2803b0cb5aacf2
bousmaha <bousmaha@A17>
2019-09-18 17:40:04 +0200

    saisiejoueur

--- MFCApplication1/MFCApplication1/SaisieDeJoueur.cpp
+++ MFCApplication1/MFCApplication1/SaisieDeJoueur.cpp
@@ -13,6 +13,10 @@ IMPLEMENT_DYNAMIC(SaisieDeJoueur, CDialogEx)
 
 SaisieDeJoueur::SaisieDeJoueur(CWnd* pParent /*=NULL*/)
 	: CDialogEx(IDD_SAISIEDEJOUEUR, pParent)
+	, NomJoueur1(_T(""))
+	, NomJoueur2(_T(""))
+	, NomJoueur3(_T(""))
+	, NomJoueur4(_T(""))
 {
 
 }
@@ -24,11 +28,59 @@ SaisieDeJoueur::~SaisieDeJoueur()
 void SaisieDeJoueur::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1, NomJoueur1);
+	DDX_Text(pDX, IDC_EDIT2, NomJoueur2);
+	DDX_Text(pDX, IDC_EDIT4, NomJoueur3);
+	DDX_Text(pDX, IDC_EDIT3, NomJoueur4);
 }
 
 
 BEGIN_MESSAGE_MAP(SaisieDeJoueur, CDialogEx)
+	ON_EN_CHANGE(IDC_EDIT4, &SaisieDeJoueur::OnEnChangeEdit4)
+	ON_EN_CHANGE(IDC_EDIT1, &SaisieDeJoueur::OnEnChangeEdit1)
+	ON_BN_CLICKED(IDOK, &SaisieDeJoueur::OnBnClickedOk)
 END_MESSAGE_MAP()
 
 
 // Gestionnaires de messages de SaisieDeJoueur
+
+
+void SaisieDeJoueur::OnEnChangeEdit4()
+{
+	// TODO:  S'il s'agit d'un contrôle RICHEDIT, le contrôle ne
+	// envoyez cette notification sauf si vous substituez CDialogEx::OnInitDialog()
+	// fonction et appelle CRichEditCtrl().SetEventMask()
+	// avec l'indicateur ENM_CHANGE ajouté au masque grâce à l'opérateur OR.
+
+	// TODO:  Ajoutez ici le code de votre gestionnaire de notification de contrôle
+}
+
+
+void SaisieDeJoueur::OnEnChangeEdit1()
+{
+	// TODO:  S'il s'agit d'un contrôle RICHEDIT, le contrôle ne
+	// envoyez cette notification sauf si vous substituez CDialogEx::OnInitDialog()
+	// fonction et appelle CRichEditCtrl().SetEventMask()
+	// avec l'indicateur ENM_CHANGE ajouté au masque grâce à l'opérateur OR.
+
+	// TODO:  Ajoutez ici le code de votre gestionnaire de notification de contrôle
+}
+
+
+void SaisieDeJoueur::OnBnClickedOk()
+{
+	// TODO: ajoutez ici le code de votre gestionnaire de notification de contrôle
+	//CDialogEx::OnOK();
+	UpdateData(true);
+	std::string j1 =(CStringA) NomJoueur1.GetBuffer();
+	std::string j2 = (CStringA)NomJoueur1.GetBuffer();
+	std::string j3 = (CStringA)NomJoueur1.GetBuffer();
+	std::string j4 = (CStringA)NomJoueur1.GetBuffer();
+
+	lesJoueurs[0] = new CJoueur(j1, 0);
+	lesJoueurs[1] = new CJoueur(j2, 0);
+	lesJoueurs[2] = new CJoueur(j3, 0);
+	lesJoueurs[3] = new CJoueur(j4, 0);
+
+}
+
--- MFCApplication1/MFCApplication1/SaisieDeJoueur.h
+++ MFCApplication1/MFCApplication1/SaisieDeJoueur.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "CJoueur.h"
 
 // Boîte de dialogue SaisieDeJoueur
 
@@ -20,4 +20,15 @@ protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // Prise en charge de DDX/DDV
 
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnEnChangeEdit4();
+	afx_msg void OnEnChangeEdit1();
+	CString NomJoueur1;
+	CString NomJoueur2;
+	CString NomJoueur3;
+	CString NomJoueur4;
+	afx_msg void OnBnClickedOk();
+private:
+	CJoueur *lesJoueurs[4];
+	
 };
